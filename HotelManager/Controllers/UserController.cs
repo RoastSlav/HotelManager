1cf020a4e2d86f15eebbe041704eba4bbcea24cf
rostislav raykov <raikov0411@gmail.com>
2021-04-07 23:17:05 +0300

    mnogo rabota i malko progres po edituser i oshte drugi neshta

---
+++ HotelManager/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using HotelManager.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelManager.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public UserController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+        
+        [HttpGet]
+        public IActionResult EditUser()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult EditUser(EditUserViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                IdentityUser identityUser = new IdentityUser();
+                identityUser.UserName = User.Identity.Name;
+                return;
+            }
+            return;
+        }
+    }
+}
