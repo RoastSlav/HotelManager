e41fe00b5543c89de8023c4b3a0b3ee03bae9e2f
denischevJDM <denischevJDM@gmail.com>
2021-04-07 15:59:02 +0300

    Clasovete za modelite

---
+++ HotelManager/Models/Room.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelManager.Models
+{
+    public class Room
+    {
+        [Required]
+        public int RoomNumber { get; set; }
+
+        [Required]
+        public int Capacity { get; set; }
+
+        [Required]
+        public string RoomType { get; set; }
+
+        public bool Vacancy { get; set; }
+
+        public decimal PriceForAdult { get; set; }
+
+        public decimal PriceForNonAdult { get; set; }
+    }
+}
